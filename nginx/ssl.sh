b80c5f9949b8ce0fdac9a2088647ca246d437209
goofynugtz <rahulranjan25.RR@gmail.com>
2023-04-29 19:27:38 +0530

    minor changes

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # mkdir -p /var/www/html/
-certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai --register-unsafely-without-email
+# certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai --register-unsafely-without-email
 while true; do sleep 1; done
