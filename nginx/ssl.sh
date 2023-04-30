bf80e89bfd9fa986da1c4a71b936e24f8a138b93
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-01 01:10:59 +0530

    Added SSL

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-# mkdir -p /var/www/html/
-# certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai --register-unsafely-without-email
+
+apt-get update
+apt-get install python3-certbot-nginx -y
+
+certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai -d rahul-prometheus.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-cadvisor.sr.flipr.ai -d rahul-node.sr.flipr.ai --register-unsafely-without-email
 while true; do sleep 1; done
