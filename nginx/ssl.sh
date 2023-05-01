4d367ede15a473af77a1d46291549227d0509ac6
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-01 22:10:06 +0530

    Minor changes

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -3,5 +3,5 @@
 apt-get install python3-certbot-nginx -y
 
 # certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai -d rahul-prometheus.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-cadvisor.sr.flipr.ai -d rahul-node.sr.flipr.ai --register-unsafely-without-email
-certbot --nginx --non-interactive --agree-tos --email rahulranjan25.RR@gmail.com --no-eff-email -d rahul-api.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-jenkins.sr.flipr.ai
+certbot --nginx --non-interactive --agree-tos -d rahul-api.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-jenkins.sr.flipr.ai --register-unsafely-without-email
 # while true; do sleep 1; done
