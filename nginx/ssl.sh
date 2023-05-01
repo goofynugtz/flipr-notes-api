e80d7fa9ff4e829c1c2ccbb4e739958e5c855220
goofynugtz <rahulranjan25.RR@gmail.com>
2023-05-02 00:52:20 +0530

    refactored for SSL

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -2,6 +2,8 @@
 
 apt-get install python3-certbot-nginx -y
 
-# certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai -d rahul-prometheus.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-cadvisor.sr.flipr.ai -d rahul-node.sr.flipr.ai --register-unsafely-without-email
-certbot --nginx --non-interactive --agree-tos -d rahul-api.sr.flipr.ai -d rahul-grafana.sr.flipr.ai -d rahul-jenkins.sr.flipr.ai --register-unsafely-without-email
-# while true; do sleep 1; done
+certbot certonly --nginx --noninteractive --agree-tos -d rahul-api.sr.flipr.ai --register-unsafely-without-email
+certbot certonly --nginx --noninteractive --agree-tos -d rahul-grafana.sr.flipr.ai --register-unsafely-without-email
+certbot certonly --nginx --noninteractive --agree-tos -d rahul-jenkins.sr.flipr.ai --register-unsafely-without-email
+
+cp default.conf.backup /etc/nginx/conf.d/default.conf
